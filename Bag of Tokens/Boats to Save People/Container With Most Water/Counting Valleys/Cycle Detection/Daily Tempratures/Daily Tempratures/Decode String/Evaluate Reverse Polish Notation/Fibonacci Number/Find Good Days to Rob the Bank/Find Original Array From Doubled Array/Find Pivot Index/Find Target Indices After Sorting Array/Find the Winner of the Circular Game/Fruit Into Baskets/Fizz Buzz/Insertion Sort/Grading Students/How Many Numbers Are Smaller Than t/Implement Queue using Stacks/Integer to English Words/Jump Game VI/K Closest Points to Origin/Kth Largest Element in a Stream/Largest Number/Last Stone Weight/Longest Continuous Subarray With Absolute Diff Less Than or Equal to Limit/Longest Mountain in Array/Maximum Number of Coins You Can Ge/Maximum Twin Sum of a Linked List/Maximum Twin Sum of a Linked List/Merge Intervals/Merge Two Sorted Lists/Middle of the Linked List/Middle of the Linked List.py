e3131391732c1f74d3ef32f475f32b049e8e1c4f
GENENE MOKONA <tisegenene@gmail.com>
2023-01-11 01:32:21 +0300

    Create Middle of the Linked List.py

---
+++ Bag of Tokens/Boats to Save People/Container With Most Water/Counting Valleys/Cycle Detection/Daily Tempratures/Daily Tempratures/Decode String/Evaluate Reverse Polish Notation/Fibonacci Number/Find Good Days to Rob the Bank/Find Original Array From Doubled Array/Find Pivot Index/Find Target Indices After Sorting Array/Find the Winner of the Circular Game/Fruit Into Baskets/Fizz Buzz/Insertion Sort/Grading Students/How Many Numbers Are Smaller Than t/Implement Queue using Stacks/Integer to English Words/Jump Game VI/K Closest Points to Origin/Kth Largest Element in a Stream/Largest Number/Last Stone Weight/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/Longest Mountain in Array/Maximum Number of Coins You Can Ge/Maximum Twin Sum of a Linked List/Maximum Twin Sum of a Linked List/Merge Intervals/Merge Two Sorted Lists/Middle of the Linked List/Middle of the Linked List.py
@@ -0,0 +1,14 @@
+class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        low = head
+        high = head
+
+        while high and high.next:
+            low = low.next
+            high = high.next.next
+
+        return low
