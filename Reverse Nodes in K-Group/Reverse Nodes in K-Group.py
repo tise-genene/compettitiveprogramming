2008c627c74ff0ff47e5a02ffe122f9a6153baf4
GENENE MOKONA <tisegenene@gmail.com>
2023-02-03 19:01:17 +0300

    Create Reverse Nodes in K-Group.py

---
+++ Reverse Nodes in K-Group/Reverse Nodes in K-Group.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        sum = 0
+        pointed = head
+
+        while(pointed):
+            sum += 1
+            pointed = pointed.next
+
+
+        prev = None
+        pointed = head
+        count = 0
+        while(pointed and count < k ):
+            next = pointed.next
+            pointed.next = prev
+            prev = pointed
+
+            pointed = next
+            count += 1
+
+
+        sum -= k
+        if sum >= k:
+            head.next = self.reverseKGroup(next,k)
+
+        else:
+            head.next = next
+
+        return prev
