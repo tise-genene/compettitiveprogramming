dd75d9025e906db7dc42c4a28ef64db49cf3ecef
GENENE MOKONA <tisegenene@gmail.com>
2023-02-05 01:09:38 +0300

    Create Create Maximum Twin Sum of a Linked List.py

---
+++ Create Maximum Twin Sum of a Linked List/Create Maximum Twin Sum of a Linked List.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def pairSum(self, head):
+        def reverseList(head):
+            prev = None
+            while head:
+                next = head.next
+                head.next = prev
+                prev = head
+                head = next
+            return prev
+
+        ans = 0
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+
+        tail = reverseList(slow)
+
+        while tail:
+            ans = max(ans, head.val + tail.val)
+            head = head.next
+            tail = tail.next
+
+        return ans
